bf39e30f5a29fc2d9dad8a3d129e582652732571
Volodymyr Stupakov <v.stupakov@sana-commerce.com>
2020-04-18 13:42:23 +0300

    add db config

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+//middlewares
 app.use(bodyParser.json());
 
-app.listen(3000, () => console.log(`Server running on ${3000} post.`));
+//database
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`Database connected`))
+  .catch((err) => console.log(`Database connection error ${err}`));
+
+//running
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server running on ${port} port`);
+});
