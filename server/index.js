ee9bad507217b52192af19388689be42931dd292
Volodymyr Stupakov <v.stupakov@sana-commerce.com>
2020-04-18 13:25:56 +0300

    general structure

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.listen(3000, () => console.log(`Server running on ${3000} post.`));
