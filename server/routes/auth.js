6cba3530d51ff26733aad39534d0d14612a86fac
Volodymyr Stupakov <v.stupakov@sana-commerce.com>
2020-04-25 13:36:01 +0300

    add express validator

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const { authController } = require('../controllers');
+const {
+  userSignupValidator,
+  userSignInValidator,
+  refreshTokensValidator,
+} = require('../validators/auth');
+const { runValidation } = require('../validators');
 
-router.post('/signup', authController.signUp);
-router.post('/signin', authController.signIn);
-router.post('/refresh-tokens', authController.refreshTokens);
+router.post(
+  '/signup',
+  userSignupValidator,
+  runValidation,
+  authController.signUp,
+);
+router.post(
+  '/signin',
+  userSignInValidator,
+  runValidation,
+  authController.signIn,
+);
+router.post(
+  '/refresh-tokens',
+  refreshTokensValidator,
+  runValidation,
+  authController.refreshTokens,
+);
 router.get('/verifyemail/:token', authController.verifyEmail);
 
 module.exports = router;
---
+++ server/validators/auth.js
@@ -0,0 +1,25 @@
+const { check } = require('express-validator');
+
+exports.userSignupValidator = [
+  check('name', 'Name is empty').not().isEmpty(),
+  check('email', 'Invalid email').isEmail(),
+  check('password', 'Invalid password').isLength({ min: 6 }),
+  check(
+    'passwordconf',
+    'passwordConfirmation field must have the same value as the password field',
+  )
+    .exists()
+    .custom((value, { req }) => value === req.body.password),
+  check('fingerprint', 'fingerprint is empty').not().isEmpty(),
+];
+
+exports.userSignInValidator = [
+  check('name', 'Name is empty').not().isEmpty(),
+  check('email', 'Invalid email').isEmail(),
+  check('password', 'Invalid password').isLength({ min: 6 }),
+  check('fingerprint', 'fingerprint is empty').not().isEmpty(),
+];
+
+exports.refreshTokensValidator = [
+  check('fingerprint', 'fingerprint is empty').not().isEmpty(),
+];
