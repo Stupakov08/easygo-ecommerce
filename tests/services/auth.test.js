9317812d35f04fca9f544b35fe43c46eaca48efc
Volodymyr Stupakov <v.stupakov@sana-commerce.com>
2020-06-19 17:12:40 +0300

    removed comments

--- tests/services/auth.test.js
+++ tests/services/auth.test.js
@@ -22,43 +22,6 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-// describe('signUp', () => {
-//   it('adds new user and return valid tokens', () => {
-//     expect.assertions(2);
-//     return authServices
-//       .signUp({
-//         email: `${uuidv4().slice(0, 8)}_stupakov@gmail.com`,
-//         password: 'myPassword',
-//         passwordconf: 'myPassword',
-//         fingerprint: 'abcd',
-//         name: 'Vova',
-//       })
-//       .then(({ accessToken, refreshToken }) => {
-//         refreshToken = jwt.verify(refreshToken, process.env.JWT_SECRET_REFRESH);
-//         accessToken = jwt.verify(accessToken, process.env.JWT_SECRET_ACCESS);
-
-//         expect(typeof refreshToken.id).toBe('string');
-//         expect(typeof accessToken.userId).toBe('string');
-//       });
-//   });
-
-//   it('handle exist email and throw EmailIsAlreadyTakenError', () => {
-//     expect.assertions(1);
-
-//     return authServices
-//       .signUp({
-//         email: 'test@gmail.com',
-//         password: 'myPass',
-//         passwordconf: 'myPass',
-//         fingerprint: 'abcdefghjklmop',
-//         name: 'Username',
-//       })
-//       .catch(err => {
-//         expect(err instanceof EmailIsAlreadyTakenError).toBe(true);
-//       });
-//   });
-// });
-
 describe('signIn', () => {
   it('returns valid jwt tokens', () => {
     expect.assertions(2);
@@ -91,24 +54,3 @@ describe('signIn', () => {
       });
   });
 });
-
-// describe('refreshToken', () => {
-//   it('returns valid jwt tokens', () => {
-//     expect.assertions(2);
-
-//     const mockedUserId = 'mockeduserid';
-//     const mockedFingerPrint = 'mockedfingerprint';
-
-//     return Session.addSession(mockedUserId, mockedFingerPrint)
-//       .then(({ refreshToken }) =>
-//         authServices.refreshTokens(refreshToken, mockedFingerPrint),
-//       )
-//       .then(({ accessToken, refreshToken }) => {
-//         refreshToken = jwt.verify(refreshToken, process.env.JWT_SECRET_REFRESH);
-//         accessToken = jwt.verify(accessToken, process.env.JWT_SECRET_ACCESS);
-
-//         expect(typeof refreshToken.id).toBe('string');
-//         expect(typeof accessToken.userId).toBe('string');
-//       });
-//   });
-// });
